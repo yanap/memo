0f765583f5a351d31ef3dae2530531a747540f21
masahiro yanai <yanai_masahiro@cyberagent.co.jp>
2013-12-13 11:32:44 +0900

    fixed path

--- sh/rbenv_install.sh
+++ sh/rbenv_install.sh
@@ -6,9 +6,9 @@ cd /usr/local/rbenv
 mkdir /usr/local/rbenv/shims /usr/local/rbenv/versions
 groupadd deployer
 chown -R root:deployer /user/local/rbenv/shims
-chmod -R 0775 /user/local/rbenv/shims
-chown -R root:deployer /user/local/rbenv/versions
-chmod -R 0775 /user/local/rbenv/versions
+chmod -R 0775 /usr/local/rbenv/shims
+chown -R root:deployer /usr/local/rbenv/versions
+chmod -R 0775 /usr/local/rbenv/versions
 cp /root/rbenv.sh /etc/profile.d/
 chmod 0755 /etc/profile.d/rbenv.sh
 
